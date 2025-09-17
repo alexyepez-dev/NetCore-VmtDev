9efd9e2102d886c747b40d06c1d856175fa5c1e2
Alex Yepez <alexdyepez@gmail.com>
2025-09-17 16:41:28 -0500

    Create VMT.ERP.MongoLibrary

---
+++ VMT.ERP/src/VMT.ERP.MongoLibrary/Models/WorkProcess/WorkProcessServiceMongo.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace VMT.ERP.MongoLibrary.Models.WorkProcess
+{
+    public class WorkProcessServiceMongo
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public object? Data { get; set; }
+        public string? Process {  get; set; }
+
+        [BsonElement("DateTime")]
+        public DateTime Date { get; set; }
+        [BsonElement("DateTimeProcess")]
+        public DateTime DateProcess { get; set; }
+    }
+}
